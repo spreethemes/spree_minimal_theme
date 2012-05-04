ec5109c01e75cf4ed6dd6301bc0c3b8fbc79dd55
Spree Themes <github@spreethemes.com>
2012-05-04 13:06:44 +0100

    updates

--- app/assets/javascripts/store/spree_minimal_theme.js
+++ app/assets/javascripts/store/spree_minimal_theme.js
@@ -1,1 +1,17 @@
 //= require store/spree_core
+jQuery( function($) {
+  if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
+      var onMouseOutOpacity = 1;
+      $('ul.product-listing > li.list-item').css('opacity', onMouseOutOpacity)
+        .hover(
+          function () {
+            $(this).prevAll().stop().fadeTo('slow', 0.60);
+            $(this).nextAll().stop().fadeTo('slow', 0.60);
+          },
+          function () {
+            $(this).prevAll().stop().fadeTo('slow', onMouseOutOpacity);
+            $(this).nextAll().stop().fadeTo('slow', onMouseOutOpacity);
+          }
+      );
+  }
+});
