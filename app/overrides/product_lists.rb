253cbe54ee8dbfd025e1a473f03bd34e244213e3
Spree Themes <github@spreethemes.com>
2012-05-11 12:20:38 +0100

    updates

--- app/overrides/product_lists.rb
+++ app/overrides/product_lists.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
                      :add_to_attributes => 'li',
                      :attributes => { :class => 'list-item' })
 
-Deface::Override.new(:virtual_path => 'spree/shared/_products',
-					 :name => 'add_add_to_cart_link',
-					 :insert_after => 'span.price',
-					 :text => "<%= link_to 'Add to Cart', add_to_cart_path(:variant_id => product.master.id), :remote => true, :method => :put %>")
+#Deface::Override.new(:virtual_path => 'spree/shared/_products',
+#					 :name => 'add_add_to_cart_link',
+#					 :insert_after => 'span.price',
+#					 :text => "<%= link_to 'Add to Cart', add_to_cart_path(:variant_id => product.master.id), :remote => true, :method => :put %>")
